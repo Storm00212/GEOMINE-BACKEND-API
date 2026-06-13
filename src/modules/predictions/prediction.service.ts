6482b2d4d878d6712094431453083e256c1a306b
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:04:02 +0300

    prediction service

---
+++ src/modules/predictions/prediction.service.ts
@@ -0,0 +1,29 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import { PredictionEngine } from './prediction-engine.js';
+// import { overloadRule } from './rules/overload.rule.js';
+// import { runtimeRule } from './rules/runtime.rule.js';
+// import { temperatureRule } from './rules/temperature.rule.js';
+// import { getSensorReadingsForBallMill } from '@modules/sensors/sensor.repository.js';
+// import { HttpError } from '@shared/errors/http.error.js';
+//
+// const engine = new PredictionEngine([overloadRule, runtimeRule, temperatureRule]);
+//
+// export const getPredictionsForBallMill = async (ballMillId: string) => {
+//   const readings = await getSensorReadingsForBallMill(ballMillId);
+//   if (!readings.length) {
+//     throw new HttpError(404, 'No sensor readings found');
+//   }
+//
+//   const latest = readings[0];
+//   const actions = engine.run({
+//     temperature: latest.temperature,
+//     currentAmp: latest.currentAmp,
+//     runtimeHours: latest.runtimeHours,
+//   });
+//
+//   return {
+//     ballMillId,
+//     latestReading: latest,
+//     predictions: actions,
+//   };
+// };
