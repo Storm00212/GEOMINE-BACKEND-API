fcab91f190fb56fb930bd494b0e3058bf4d098bb
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:32:44 +0300

    COMMENTS

--- src/shared/utils/hash.util.ts
+++ src/shared/utils/hash.util.ts
@@ -1,4 +1,9 @@
-// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// FILE PURPOSE:
+// - Provides password hashing utilities.
+//
+// NOTE: This file currently contains paused implementation code for team review.
+//
+
 // import bcrypt from 'bcryptjs';
 // import { config } from '@config/env';
 //
