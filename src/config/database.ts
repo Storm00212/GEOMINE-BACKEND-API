0bd9c52713c13488298894ac7296134d1034aaab
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:24:31 +0300

    COMMENTS

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,9 @@
+// FILE PURPOSE:
+// - Initializes the Prisma database client used by the application.
+//
+// NOTE: This file currently contains paused implementation code for team review.
+//
+
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
