206115fd8cb9bd3071211c5426d13a892abdafe4
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:04:56 +0300

    temperature rule

---
+++ src/modules/predictions/rules/temperature.rule.ts
@@ -0,0 +1,7 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import { PredictionRule } from '../prediction-engine.js';
+//
+// export const temperatureRule: PredictionRule = {
+//   matches: ({ temperature }) => temperature > 75,
+//   action: 'GREASING_REQUIRED',
+// };
