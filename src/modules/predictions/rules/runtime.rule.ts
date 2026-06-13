55457bdfa9ab335bffad033bbd1ba564b3ad0105
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:04:42 +0300

    runtime rule

---
+++ src/modules/predictions/rules/runtime.rule.ts
@@ -0,0 +1,7 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import { PredictionRule } from '../prediction-engine.js';
+//
+// export const runtimeRule: PredictionRule = {
+//   matches: ({ runtimeHours }) => runtimeHours > 300,
+//   action: 'PREVENTIVE_MAINTENANCE_DUE',
+// };
