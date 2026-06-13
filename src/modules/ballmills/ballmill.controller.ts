e9beeef767ce79a4a8add09e4934df8d6d3ff697
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:00:12 +0300

    ballmill controller

---
+++ src/modules/ballmills/ballmill.controller.ts
@@ -0,0 +1,39 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import { Request, Response, NextFunction } from 'express';
+// import { createBallMill, getBallMill, listBallMills, updateBallMill } from './ballmill.service.js';
+//
+// export const getBallMillsHandler = async (_req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const ballMills = await listBallMills();
+//     res.status(200).json(ballMills);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+//
+// export const getBallMillHandler = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const ballMill = await getBallMill(req.params.id);
+//     res.status(200).json(ballMill);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+//
+// export const createBallMillHandler = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const ballMill = await createBallMill(req.body);
+//     res.status(201).json(ballMill);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+//
+// export const updateBallMillHandler = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const ballMill = await updateBallMill(req.params.id, req.body);
+//     res.status(200).json(ballMill);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
