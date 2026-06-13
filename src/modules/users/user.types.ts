1d256bf72f7ca346c0189aa27fa5d99facdc335b
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:07:21 +0300

    user types

---
+++ src/modules/users/user.types.ts
@@ -0,0 +1,9 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import { Role } from '@shared/constants/roles';
+//
+// export type UserCreatePayload = {
+//   name: string;
+//   email: string;
+//   password: string;
+//   role: Role;
+// };
