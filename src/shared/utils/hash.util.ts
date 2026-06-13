93d59e0e3dddbaddcc004fa33bbd0c5c470aac41
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 14:08:45 +0300

    hash util for password hashing

---
+++ src/shared/utils/hash.util.ts
@@ -0,0 +1,6 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import bcrypt from 'bcryptjs';
+// import { config } from '@config/env';
+//
+// export const hashPassword = async (password: string) => bcrypt.hash(password, config.saltRounds);
+// export const comparePassword = async (password: string, hash: string) => bcrypt.compare(password, hash);
