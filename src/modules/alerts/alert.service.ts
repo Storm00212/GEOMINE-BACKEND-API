da8fd761ada3ff6cbd28d42e0c6abdac3dd4dab0
Storm00212 <paulmuyalikhams@gmail.com>
2026-06-13 13:58:56 +0300

    alert service

---
+++ src/modules/alerts/alert.service.ts
@@ -0,0 +1,4 @@
+// FILE COMMENTED OUT FOR TEAM REVIEW: active implementation paused except db/env setup
+// import { getAllAlerts } from './alert.repository.js';
+//
+// export const listAlerts = () => getAllAlerts();
