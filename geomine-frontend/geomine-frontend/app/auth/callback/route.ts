67079d4366726fef36a1065ab7628b3ea5f4f599
Storm00212 <paulmuyalikhams@gmail.com>
2026-07-11 00:50:51 +0300

    nate

--- geomine-frontend/geomine-frontend/app/auth/callback/route.ts
+++ geomine-frontend/geomine-frontend/app/auth/callback/route.ts
@@ -4,11 +4,14 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
+  const type = searchParams.get("type");
 
   if (code) {
     const supabase = createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(origin);
+  const nextPath = type === "recovery" ? "/reset-password" : "/";
+
+  return NextResponse.redirect(new URL(nextPath, origin));
 }
