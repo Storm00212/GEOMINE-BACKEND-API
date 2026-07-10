d52d11ee40f6ddf4c5e8b759ea34cc84f97c2122
Storm00212 <paulmuyalikhams@gmail.com>
2026-07-11 02:41:56 +0300

    FIXES

--- geomine-backend/geomine-backend/app/api/machines/route.ts
+++ geomine-backend/geomine-backend/app/api/machines/route.ts
@@ -1,4 +1,5 @@
 import { listMachinesController, createMachineController } from "@/lib/modules/machines/machines.controller";
 
+export const dynamic = "force-dynamic";
 export const GET = listMachinesController;
 export const POST = createMachineController;
